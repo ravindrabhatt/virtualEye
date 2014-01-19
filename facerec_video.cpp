fc38605cfb4859ba3f27ae5f54dc9ab61f5d73ba
ravindrabhatt <ravindra.bhatt29@gmail.com>
2014-01-19 11:46:54 +0530

    name of recognised person is now shown

--- facerec_video.cpp
+++ facerec_video.cpp
@@ -1,4 +1,3 @@
-
 #include "opencv2/core/core.hpp"
 #include "opencv2/contrib/contrib.hpp"
 #include "opencv2/highgui/highgui.hpp"
@@ -12,8 +11,19 @@
 using namespace cv;
 using namespace std;
 
+
+//map labels to people name
+typedef struct recogniseMap
+{
+    string name;
+    int label;
+    recogniseMap * next;
+}recogniseMap;
+
+
 static void read_csv(const string& filename, vector<Mat>& images, vector<int>& labels, char separator = ';') {
     std::ifstream file(filename.c_str(), ifstream::in);
+    int i=1;
     if (!file) {
         string error_message = "No valid input file was given, please check the given filename.";
         CV_Error(CV_StsBadArg, error_message);
@@ -30,20 +40,72 @@ static void read_csv(const string& filename, vector<Mat>& images, vector<int>& l
     }
 }
 
+recogniseMap* readPeople(const string& filename, recogniseMap * map){
+    std::ifstream file(filename.c_str(), ifstream::in);
+    recogniseMap *mapTemp = NULL;
+    if (!file) {
+        string error_message = "No valid input file was given, please check the given filename.";
+        CV_Error(CV_StsBadArg, error_message);
+    }
+
+    string line, name, label;
+    while(getline(file, line)){
+        stringstream liness(line);
+        getline(liness, name, ' ');
+        getline(liness, label);
+        if(!name.empty() && !label.empty()) {
+            if(mapTemp == NULL){
+                map = new recogniseMap();
+                mapTemp = map;
+            }
+            else{
+                map->next = new recogniseMap();
+                map = map->next;
+            }    
+            map->name = string(name);
+            map->label = atoi(label.c_str());
+            map->next = NULL;
+            
+            
+        }
+    }
+    map = mapTemp;
+    return map;
+}
+
+string getName(const int label, recogniseMap * map){
+    recogniseMap * tmap = map;
+    string name = string("p");
+
+    while(tmap){
+        if(tmap->label == label){
+            name = string(tmap->name);
+            break;
+        }
+        tmap = tmap->next;
+    }
+
+    return name;
+}
+
 int main(int argc, const char *argv[]) {
     // Check for valid command line arguments, print usage
     // if no arguments were given.
-    if (argc != 4) {
+    if (argc != 5) {
         cout << "usage: " << argv[0] << " </path/to/haar_cascade> </path/to/csv.ext> </path/to/device id>" << endl;
         cout << "\t </path/to/haar_cascade> -- Path to the Haar Cascade for face detection." << endl;
         cout << "\t </path/to/csv.ext> -- Path to the CSV file with the face database." << endl;
         cout << "\t <device id> -- The webcam device id to grab frames from." << endl;
+        cout << "\t <name mapping file> File which contains mapping of name to label." << endl;
         exit(1);
     }
     // Get the path to your CSV:
     string fn_haar = string(argv[1]);
     string fn_csv = string(argv[2]);
     int deviceId = atoi(argv[3]);
+    string peopleName = string(argv[4]);
+
+
     // These vectors hold the images and corresponding labels:
     vector<Mat> images;
     vector<int> labels;
@@ -55,6 +117,12 @@ int main(int argc, const char *argv[]) {
         // nothing more we can do
         exit(1);
     }
+    
+    
+    //read name of the people known
+    recogniseMap *map = NULL;
+    map = readPeople(peopleName, map);
+ 
     // Get the height from the first image. We'll need this
     // later in code to reshape the images to their original
     // size AND we need to reshape incoming faces to this size:
@@ -73,6 +141,7 @@ int main(int argc, const char *argv[]) {
         return -1;
     }
     // Holds the current frame from the Video device:
+
     Mat frame;
     for(;;) {
         cap >> frame;
@@ -84,6 +153,7 @@ int main(int argc, const char *argv[]) {
         // Find the faces in the frame:
         vector< Rect_<int> > faces;
         haar_cascade.detectMultiScale(gray, faces);
+    
         // At this point you have the position of the faces in
         // faces. Now we'll get the faces, make a prediction and
         // annotate it in the video. 
@@ -92,6 +162,7 @@ int main(int argc, const char *argv[]) {
             Rect face_i = faces[i];
             // Crop the face from the image. So simple with OpenCV C++:
             Mat face = gray(face_i);
+
             // Resizing the face is necessary for Eigenfaces and Fisherfaces.
             Mat face_resized;
             cv::resize(face, face_resized, Size(im_width, im_height), 1.0, 1.0, INTER_CUBIC);
@@ -101,7 +172,8 @@ int main(int argc, const char *argv[]) {
             // First of all draw a green rectangle around the detected face:
             rectangle(original, face_i, CV_RGB(0, 255,0), 1);
             // Create the text we will annotate the box with:
-            string box_text = format("Prediction = %d", prediction);
+            string res = getName(prediction, map);
+            string box_text = format("%s", res.c_str());
             // Calculate the position for annotated text (make sure we don't
             // put illegal values in there):
             int pos_x = std::max(face_i.tl().x - 10, 0);
